b44b05ad9bbb7acad4d91760a70cea71c526c6b5
Choonick <wecube.1@gmail.com>
2018-05-11 01:10:39 +0900

    update

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import io.swagger.annotations.*;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -16,20 +17,39 @@ import java.util.concurrent.atomic.AtomicLong;
 
 
 @RestController
+@RequestMapping("/Greeting")
+
+@Api(value="onlinestore", description="물건 찾기 API")
 public class GreetingController {
     private static final String template = "Hello, %s";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ApiOperation(value = "분실물 목록")
+//    @ApiResponses(value = {
+//            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+//            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+//            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+//            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+//    }
+//    )
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "category", value = "분실물 종류", required = true, dataType = "string", paramType = "query", defaultValue = "핸드폰"),
+            @ApiImplicitParam(name = "place", value = "분실 장소", required = true, dataType = "string", paramType = "query", defaultValue = "b1"),
+            @ApiImplicitParam(name = "name", value = "분실물 명", dataType = "string", paramType = "query", defaultValue = ""),
+    })
+
+
+    @RequestMapping(value = "/losts", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public List<JSONObject> restTemplate(HttpServletRequest request) throws Exception {
         String baseUrl = "openapi.seoul.go.kr";
         String svcKey = "72694d7657737769323442486a694d";
-        String name = request.getParameter("name");
         String category = request.getParameter("category");
         String place = request.getParameter("place");
+        String name = request.getParameter("name");
 
-        System.out.println(name+category+place);
+        if(name==null) name = "";
 
         RestTemplate restTpl = new RestTemplate();
         URI uri = UriComponentsBuilder.newInstance()
@@ -37,10 +57,12 @@ public class GreetingController {
                 .host(baseUrl)
                 .port("8088")
                 .path(svcKey)
-                .path("/json/SearchLostArticleService/1/8/핸드폰/b1")
-//                .path(category)
-//                .path(place)
-//                .path(name)
+                .path("/json/SearchLostArticleService/1/8/")
+                .path(category)
+                .path("/")
+                .path(place)
+                .path("/")
+                .path(name)
                 .build()
                 .encode()
                 .toUri();
@@ -53,6 +75,7 @@ public class GreetingController {
         *
         * loging과 System.out.println의 차이점
         */
+
 
         String responseStr = restTpl.getForObject(uri, String.class);
 
@@ -95,9 +118,6 @@ public class GreetingController {
         JSONObject row = (JSONObject) rows.get(0);
 
         String url = (String) row.get("IMAGE_URL");
-
-
-        System.out.println(url);
 
         return url;
 
--- src/main/java/com/example/demo/homeController.java
+++ src/main/java/com/example/demo/homeController.java
@@ -1,6 +1,11 @@
 package com.example.demo;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +13,29 @@ public class homeController {
     @RequestMapping("/ping")
     public String home() {
         return "hello Spring Boot!";
+    }
+
+    @RequestMapping("/ping2")
+    public String home2() {
+        return "hello Spring Boot!";
+    }
+
+
+    @ApiOperation(value = "헬로우 테스트", httpMethod = "GET", notes = "헬로우 테스트입니다")
+
+    @ApiResponses(value = {
+
+            @ApiResponse(code = 400, message = "Invalid input:…"),
+
+            @ApiResponse(code = 200, message = "Ok" )
+
+    })
+
+    @RequestMapping(value="/hello", method= RequestMethod.POST )
+
+    public String hello(@ApiParam(value = "키값", required = true, defaultValue = "기본값") String value) {
+
+        return "Blind Interview Server Running\n";
+
     }
 }
